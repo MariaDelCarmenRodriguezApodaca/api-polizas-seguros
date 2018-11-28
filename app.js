597d874c08ff0d0a6d770a05cd58bc64fb89351a
MariaDelCarmenRodriguezApodaca <mdcra1997@outlook.com>
2018-11-28 00:34:07 -0700

    kjasdhak

--- app.js
+++ app.js
@@ -32,12 +32,17 @@ let pendienteRoutes = require('./routes/pendiente');
 
 //configurar cabeceras y cors
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method', 'ResponseContentType'),
-        res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE'),
-        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+     // Website you wish to allow to connect
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     // Request methods you wish to allow
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+     // Request headers you wish to allow
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+     // Set to true if you need the website to include cookies in the requests sent
+     // to the API (e.g. in case you use sessions)
+     res.setHeader('Access-Control-Allow-Credentials', true);
+     // Pass to next layer of middleware
+     next();
 });
 
 
